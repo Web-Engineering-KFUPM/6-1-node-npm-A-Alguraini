1ef7345a3e8dd420e4cad66cdf5828edd95be987
A-Alguraini <abdulaziz.alguraini@gmail.com>
2025-11-10 14:12:59 +0300

    task4

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,18 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((accumulator, current) => accumulator - current);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((accumulator, current) => accumulator * current, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (numbers.slice(1).includes(0)) {
+    return "Error: Division by zero is not allowed.";
+  }
+  return numbers.reduce((accumulator, current) => accumulator / current);
 }
-
